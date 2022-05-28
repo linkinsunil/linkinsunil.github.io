4c89dac12b7671531310f5f92fb55de938ee39fb
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-05-28 20:02:01 +0530

    update: text in homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,44 +5,40 @@ import homeAnime from './../../assets/home_anime.gif';
 import './Home.css';
 
 class Home extends Component {
-    state = { showModal: false };
+  state = { showModal: false };
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({ showModal: false });
-        }, 1000);
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ showModal: false });
+    }, 1000);
+  }
 
-    closeModal = () => {
-        this.setState({ showModal: false });
-    };
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
 
-    render() {
-        return (
-            <div className='home-container'>
-                {this.state.showModal && <Modal closeModal={this.closeModal} />}
-                <div className='header-text'>
-                    <h1>Hi, This is Sunil</h1>
-                    <p>A frontend dev and a tech enthusiast</p>
-                </div>
-                <div className='head-btns'>
-                    <Link to='/about' className='btn btn-white'>
-                        <p className='btn-text'>Know more about me</p>
-                    </Link>
-                    <Link to='/contact' className='btn btn-transparent'>
-                        <p className='btn-text'>Connect with me</p>
-                    </Link>
-                </div>
-                <div className='splash-image'>
-                    <img
-                        src={homeAnime}
-                        alt='animation'
-                        className='home-anime'
-                    />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='home-container'>
+        {this.state.showModal && <Modal closeModal={this.closeModal} />}
+        <div className='header-text'>
+          <h1>Hi, This is Sunil</h1>
+          <p>A frontend developer and a tech enthusiast</p>
+        </div>
+        <div className='head-btns'>
+          <Link to='/about' className='btn btn-white'>
+            <p className='btn-text'>Know more about me</p>
+          </Link>
+          <Link to='/contact' className='btn btn-transparent'>
+            <p className='btn-text'>Connect with me</p>
+          </Link>
+        </div>
+        <div className='splash-image'>
+          <img src={homeAnime} alt='animation' className='home-anime' />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
