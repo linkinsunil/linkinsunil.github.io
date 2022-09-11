304c8f3fa0844e3a39c5bcb8e5d8cdc29d15cf75
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-09-11 15:37:03 +0530

    updated links and skills

--- src/assets/skillsData.js
+++ src/assets/skillsData.js
@@ -19,16 +19,24 @@ export const skillList = [
     skillName: 'Redux',
     skillUrl: 'https://madhavbahl.tech/portfolio-assets/redux.png',
   },
-  // {
-  //     skillName: 'NodeJS',
-  //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
-  // },
-  // {
-  //     skillName: 'TypeScript',
-  //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/ts.png',
-  // },
-  // {
-  //     skillName: 'MongoDB',
-  //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/mongo.png',
-  // },
+  {
+    skillName: 'NodeJS',
+    skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
+  },
+  {
+    skillName: 'MongoDB',
+    skillUrl: 'https://madhavbahl.tech/portfolio-assets/mongo.png',
+  },
+  {
+    skillName: 'Styled Components',
+    skillUrl: 'https://avatars.githubusercontent.com/u/20658825?s=200&v=4',
+  },
+  {
+    skillName: 'MUI',
+    skillUrl: 'https://avatars.githubusercontent.com/u/33663932?s=200&v=4',
+  },
+  {
+    skillName: 'Git',
+    skillUrl: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+  },
 ];
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,90 +11,73 @@ import twitter from './../../assets/twitter.png';
 import hashnode from './../../assets/hashnode.png';
 
 const Contact = () => {
-    return (
-        <div className='section-container'>
-            <Header
-                heading='Get in touch.'
-                details='Interested to collaborate? Feel free to drop me an email.'
-            />
+  return (
+    <div className='section-container'>
+      <Header
+        heading='Get in touch.'
+        details='Interested to collaborate? Feel free to drop me an email.'
+      />
 
-            {/* Form section */}
-            <div className='contact-form-container'>
-                <form
-                    className='contact-form'
-                    action="https://formspree.io/f/xayagqdj"
-                    method="POST"
-                >
-                    {/* Email Id Input */}
-                    <input
-                        type='email'
-                        placeholder='Your Email ID'
-                        name="_replyto"
-                        className='input-box email-input'
-                        autoComplete='off'
-                    />
+      {/* Form section */}
+      <div className='contact-form-container'>
+        <form
+          className='contact-form'
+          action='https://formspree.io/f/xayagqdj'
+          method='POST'
+        >
+          {/* Email Id Input */}
+          <input
+            type='email'
+            placeholder='Your Email ID'
+            name='_replyto'
+            className='input-box email-input'
+            autoComplete='off'
+          />
 
-                    {/* Email Body */}
-                    <textarea
-                        type='text'
-                        placeholder='Your Message'
-                        name='message'
-                        className='input-box body-input'
-                    ></textarea>
+          {/* Email Body */}
+          <textarea
+            type='text'
+            placeholder='Your Message'
+            name='message'
+            className='input-box body-input'
+          ></textarea>
 
-                    {/* Submit button */}
-                    <button type='submit' className='contact-btn'>
-                        Send Email
-                    </button>
-                </form>
-            </div>
+          {/* Submit button */}
+          <button type='submit' className='contact-btn'>
+            Send Email
+          </button>
+        </form>
+      </div>
 
-            {/* Social icons */}
-            <div className='social-icons-container'>
-                <a href='https://github.com/linkinsunil' className='social-icon'>
-                    <img src={github} alt='social' />
-                </a>
-                <a
-                    href='https://linkedin.com/in/linkinsunil'
-                    className='social-icon'
-                >
-                    <img src={linkedin} alt='social' />
-                </a>
-                <a
-                    href='https://instagram.com/linkinsunil'
-                    className='social-icon'
-                >
-                    <img src={instagram} alt='social' />
-                </a>
-                <a
-                    href='https://twitter.com/officialskv'
-                    className='social-icon'
-                >
-                    <img src={twitter} alt='social' />
-                </a>
-                <a href='https://linkinsunil.hashnode.dev/' className='social-icon'>
-                    <img src={hashnode} alt='social' />
-                </a>
-                <a href='https://linkinsunil.netlify.app' className='social-icon'>
-                    <img src={web} alt='social' />
-                </a>
-            </div>
+      {/* Social icons */}
+      <div className='social-icons-container'>
+        <a href='https://github.com/linkinsunil' className='social-icon'>
+          <img src={github} alt='social' />
+        </a>
+        <a href='https://linkedin.com/in/linkinsunil' className='social-icon'>
+          <img src={linkedin} alt='social' />
+        </a>
+        <a href='https://instagram.com/linkinsunil' className='social-icon'>
+          <img src={instagram} alt='social' />
+        </a>
+        <a href='https://twitter.com/officialskv' className='social-icon'>
+          <img src={twitter} alt='social' />
+        </a>
+        <a href='https://linkinsunil.hashnode.dev/' className='social-icon'>
+          <img src={hashnode} alt='social' />
+        </a>
+        <a href='https://linkinsunil.github.io' className='social-icon'>
+          <img src={web} alt='social' />
+        </a>
+      </div>
 
-            <FooterLink
-                phrase='Read more '
-                toAdress='/about'
-                link='about me.'
-            />
+      <FooterLink phrase='Read more ' toAdress='/about' link='about me.' />
 
-            <div className='vector-frame'>
-                <img
-                    src={contactVector}
-                    alt='vector'
-                    className='about-vector'
-                />
-            </div>
-        </div>
-    );
+      <div className='vector-frame'>
+        <img src={contactVector} alt='vector' className='about-vector' />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
